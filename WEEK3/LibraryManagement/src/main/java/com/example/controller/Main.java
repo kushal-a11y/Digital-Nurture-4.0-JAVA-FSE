09ec56267c56a993caa082ae266b80f17bf8a4a0
kushalstcet666@gmail.com <kushalstcet666@gmail.com>
2025-07-12 20:52:38 +0530

    main added

---
+++ WEEK3/LibraryManagement/src/main/java/com/example/controller/Main.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+import com.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main{
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service = (BookService)context.getBean("bookService");
+        service.addBook("\"Secret History of Mongols\"");
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
