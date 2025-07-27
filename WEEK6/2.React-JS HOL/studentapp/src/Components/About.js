a1ef726b9d5497e9798535c6696b697f98c700af
kushalstcet666@gmail.com <kushalstcet666@gmail.com>
2025-07-27 16:27:37 +0530

    week6

---
+++ WEEK6/2.React-JS HOL/studentapp/src/Components/About.js
@@ -0,0 +1,11 @@
+// import React, {Component} from 'react'
+
+function About(){
+    return(
+        <div>
+        <h3>Wlecome to the About Page of Student Management Portal</h3>
+    </div>
+    );
+}
+
+export default About;
