528ceee25633e643e759359ec562d231b7b70d21
Kushal Das <kushalstcet666@gmail.com>
2025-08-03 13:55:33 +0000

    exercise 10 done

--- WEEK7/10.React-JS HOL/officespacerentalapp/src/App.js
+++ WEEK7/10.React-JS HOL/officespacerentalapp/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.css";
+import officeImage from "./office.png";
 
 function App() {
+  // const office = {
+  //   name: "Skyline tower",
+  //   rent: 55000,
+  //   address: "Downtown Business Street",
+  // };
+  const offices = [
+    { id: 1, name: "Skyline Tower", rent: 55000, address: "Downtown" },
+    { id: 2, name: "Tech Park 1", rent: 70000, address: "IT Hub" },
+    { id: 3, name: "Home Park", rent: 80000, address: "Home Hub" },
+    { id: 4, name: "Noodles Park", rent: 90000, address: "Noodles Hub" },
+    { id: 5, name: "Tech Park 2", rent: 70000, address: "IT Hub" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Office Space Rental</h1>
+      {/* <img src={officeImage} alt="Office Image" width={200} />
+      <h2>Office name: {office.name}</h2>
+      <p style={{ color: office.rent < 60000 ? "red" : "greeb" }}>
+        Rent: {office.rent}
+      </p>
+      <p>Address: {office.address}</p> */}
+      <div className="OfficeListContainer">
+        {offices.map((office) => (
+          <div key={office.id} className="Office">
+            {/** A stable  id is used for each office component*/}
+            <h1>{office.name}</h1>
+            <p style={{ color: office.rent < 60000 ? "red" : "green" }}>
+              {office.rent}
+            </p>
+            <p>{office.address}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
