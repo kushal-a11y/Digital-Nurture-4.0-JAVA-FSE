2cc26e3276a2df7784388b4efd8c0cfb68ff8b50
Kushal Das <kushalstcet666@gmail.com>
2025-08-03 15:23:33 +0000

    file added

--- WEEK7/11.React-JS HOL/eventexamplesapp/src/App.js
+++ WEEK7/11.React-JS HOL/eventexamplesapp/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState("");
+  const sayHello = () => {
+    console.log("Hello from Increment");
+    setMessage("Hello from Increment");
+  };
+  const sayWelcome = (welcomeMessage) => {
+    console.log(welcomeMessage);
+    setMessage(welcomeMessage);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter: {counter}</h2>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          sayHello();
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter > 0 ? counter - 1 : 0);
+          // sayHello();
+        }}
+      >
+        Decrement
+      </button>
+      <h1>Button That Says Welcome</h1>
+      <button
+        onClick={() => sayWelcome("Welcome")}
+        style={{
+          color: "white",
+          backgroundColor: "green",
+          border: "2px solid black",
+          width: "100px",
+          marginTop: "20px",
+        }}
+      >
+        Say Welcome
+      </button>
+      {message && <p>{message}</p>}
     </div>
   );
 }
